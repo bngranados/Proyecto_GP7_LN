681ccd2df273d11ef27e9c085a819eda2e0d1a80
Brenda_Granados <134071712+NazarethGranados@users.noreply.github.com>
2025-10-20 18:40:17 -0600

    Problemas con codigo
    Solucion a problemas inesperados del codigo + commit de la implementacion de GridLayout ya que por problemas tecnicos no se pudo hacer antes.

--- Proyecto_Avance1/Juego.java
+++ Proyecto_Avance1/Juego.java
@@ -1,37 +1,65 @@
 package Proyecto_Avance1;
 
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
+import java.util.Queue;
 
 public class Juego {
 
-  
-    private List<Carta> caja = new ArrayList<>();
+    private LinkedList<Carta> caja;
+    private Stack<Carta> mazo;
+    private List<Carta> mano;
+    private Queue<Carta> pozo;
 
-    public Juego() {
+    public Juego()
+    {
+        caja = new LinkedList<>();
+        mazo = new Stack<>();
+        mano = new LinkedList<>();
+        pozo = new LinkedList<>();
         inicializarBaraja();
     }
 
-
-    private void inicializarBaraja() {
+    private void inicializarBaraja()
+    {
         String[] palos = {"♥", "♦", "♠", "♣"};
         String[] valores = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
         String[] nombres = {"As", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "Ocho", "Nueve", "Diez", "Jota", "Quina", "Ka"};
-        
+
         for (String palo : palos) {
-            
-            for (int i = 0; i < valores.length; i++) {
+            for (int i = 0; i < valores.length; i++)
+            {
                 String valor = valores[i];
                 String nombre = nombres[i];
-                int numero = i + 1; 
+                int numero = i + 1;
                 Carta carta = new Carta(palo, valor, nombre, numero);
-                caja.add(carta); 
+                caja.add(carta);
             }
         }
     }
 
-    public List<Carta> getCaja() {
+    public void barajarYPrepararMazo() {
+        mazo.addAll(caja);
+        caja.clear();
+        Collections.shuffle(mazo);
+    }
+    
+    public LinkedList<Carta> getCaja()
+    {
         return caja;
     }
-}
 
+    public List<Carta> getMano() {
+        return mano;
+    }
+    
+    public Stack<Carta> getMazo() {
+        return mazo;
+    }
+    
+    public Queue<Carta> getPozo() {
+        return pozo;
+    }
+}
