870e6b499276f4e8bc6b4db30eb9ba73108d6290
Brenda_Granados <134071712+NazarethGranados@users.noreply.github.com>
2025-11-18 21:17:39 -0600

    cambios
    clase main creada por error

--- Proyecto_Avance2/Main.java
+++
@@ -1,9 +0,0 @@
-package Proyecto_Avance2;
-
-public class Main {
-    public static void main(String[] args) {
-        InterfazJuego ventana = new InterfazJuego();
-        ventana.setVisible(true);
-        ventana.setLocationRelativeTo(null);
-    }
-}
