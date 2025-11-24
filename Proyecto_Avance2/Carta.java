5fefd0706fb5e2a2f31c47e89b2808b4a0a52a89
Mayquelin <145294597+Mayquelin@users.noreply.github.com>
2025-11-24 17:26:51 -0600

    Refactor Carta class to include Nimbus look and feel

--- Proyecto_Avance2/Carta.java
+++ Proyecto_Avance2/Carta.java
@@ -1,12 +1,15 @@
 package Proyecto_Avance2;
 
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+
 public class Carta {
 
-  private final String palo;  
-  private final String color;   
-  private final String valor;   
-  private final String nombre;  
-  private final int numero;     
+  private final String palo;
+  private final String color;
+  private final String valor;
+  private final String nombre;
+  private final int numero;
 
   public Carta(String palo, String valor, String nombre, int numero) {
     this.palo = palo;
@@ -42,14 +45,23 @@ public class Carta {
   }
 
   public static void main(String[] args) {
-    Carta carta = new Carta("♥", "A", "As", 1);
-    System.out.println(carta);
+    try {
+      for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+        if ("Nimbus".equals(info.getName())) {
+          UIManager.setLookAndFeel(info.getClassName());
+          Carta carta = new Carta("♥", "A", "As", 1);
+          System.out.println(carta);
 
-    InterfazJuego ventana = new InterfazJuego();
-    ventana.setVisible(true);
-    ventana.setLocationRelativeTo(null);
+          InterfazJuego ventana = new InterfazJuego();
+          ventana.setVisible(true);
+          ventana.setLocationRelativeTo(null);
+          break;
+        }
+      }
+    } catch (Exception e) {
+      // If Nimbus is not available, you can set the GUI to another look and feel.
+    }
 
   }
-
 
 }
